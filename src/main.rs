1068cf7cf435b20bdf3e16bf20fbaf775e32a2dd
GT-GillesTeuwen <gillesteuwen@gmail.com>
2024-10-17 09:04:44 +0200

    Meep

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use image::{GenericImageView, GrayImage, Luma};
+use rfd::FileDialog;
 use std::fs;
 use std::path::{Path, PathBuf};
-use rfd::FileDialog;
 
 mod file_writing;
 mod histogram_drawer;
@@ -9,8 +9,9 @@ mod kapur;
 mod otsu;
 mod stats;
 
-fn main() {
+fn run() -> Result<(), Box<dyn std::error::Error>> {
     // Directory containing the images
+    println!("Select the folder containing the images");
     let img_dir = FileDialog::new()
         .set_directory("/")
         .pick_folder()
@@ -34,12 +35,29 @@ fn main() {
             fs::create_dir_all(&dir_path).expect("Failed to create directory");
         }
     }
+    println!("meep");
 
     // Iterate over all files in the directory
     for entry in fs::read_dir(img_dir).expect("Failed to read directory") {
         let entry = entry.expect("Failed to read directory entry");
+
         process_file(entry);
     }
+    Ok(())
+}
+
+fn main() {
+    if let Err(e) = run() {
+        eprintln!("Application error: {}", e);
+        std::process::exit(1);
+    }
+
+    // Pause before exit
+    println!("Results saved in the results folder where this program is located");
+    println!("Run outputs saved to csv files where this program is located");
+    println!("Press Enter to close...");
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
 }
 
 fn process_file(entry: fs::DirEntry) {
@@ -56,6 +74,7 @@ fn process_file(entry: fs::DirEntry) {
 
 fn process_image(img_path: &PathBuf) {
     // Read the image
+
     let img = image::open(&img_path).expect("Failed to open image");
 
     // Convert the image to grayscale (if it's not already)
@@ -63,7 +82,7 @@ fn process_image(img_path: &PathBuf) {
 
     // Histogram exploration
     let exclude_zero = true;
-    explore_histgram(img_path, &gray_img, exclude_zero);
+    //explore_histgram(img_path, &gray_img, exclude_zero);
 
     let to_run: Vec<(
         &str,
@@ -101,6 +120,20 @@ fn process_image(img_path: &PathBuf) {
         //     otsu::compute_exhaustive_otsu_thresholds,
         // ),
     ];
+    let file_stem = img_path.file_stem().unwrap().to_string_lossy();
+    println!(
+        "Processing image {:?}\nWhat will be run:
+    \n\tOtsu, k=2,3,4,5, SA X30
+    \n\tOtsu, k=2,3,4,5, VNS X30
+    \n\tKapur, k=2,3,4,5, SA X30
+    \n\tKapur, k=2,3,4,5, VNS X30
+    \n A total of 480 runs will be performed
+    \nResults will be saved in the results folder where this program is located
+    \nPress Enter to continue...",
+        file_stem
+    );
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
     for k in 2..=5 {
         for _ in 0..30 {
             do_metric_thresholding(&to_run, img_path, &gray_img, k, exclude_zero);
@@ -157,7 +190,10 @@ pub fn do_metric_method(
         method_name.to_lowercase(),
         k
     );
-    let histogram_path = format!("{}/histogram/{}_k{}_histogram_{:?}.png", base_path, file_stem, k,thresholds);
+    let histogram_path = format!(
+        "{}/histogram/{}_k{}_histogram_{:?}.png",
+        base_path, file_stem, k, thresholds
+    );
     let segmented_path = format!("{}/{}_k{}_{:?}.png", base_path, file_stem, k, thresholds);
     draw_threshold_hist_and_save_image(
         &histogram_path,
